bdffc8b109c30d91b71c525e4e035368df1866d0
MaryEliseeva <marymaryeliseeva@gmail.com>
2025-11-17 20:55:59 +0300

    practice41

---
+++ practice41/ScannerFileExample.java
@@ -0,0 +1,20 @@
+// practice40/ScannerFileExample.java
+package practice41;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScannerFileExample {
+    public static void main(String[] args) {
+        try (Scanner fileScanner = new Scanner(new File("practice41/test_input.txt"))) {
+            System.out.println("Чтение данных из файла:");
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
+        }
+    }
+}
